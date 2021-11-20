e4eda8ff1bd5bc4db3693046076168f77e1740de
Jørund Martinsen <joerundmartinsen@hotmail.com>
2021-11-20 16:19:28 +0100

    Conf: More apps in choco install

--- config/choco-installs.ps1
+++ config/choco-installs.ps1
@@ -1,17 +1,34 @@
 function Install-Tools {
-    choco install brave nordpass nordvpn vscode microsoft-windows-terminal voicemeeter -y
+    choco install git -y 
+    choco install 7zip -y 
+    choco install brave -y 
+    choco install nordpass -y 
+    choco install nordvpn -y 
+    choco install vscode -y 
+    choco install microsoft-windows-terminal -y 
+    choco install nodejs -y
+    choco install yarn -y
+    choco install voicemeeter -y
 }
 
 function Install-WSL2 {
     choco install wsl2 --params "/Version:2 Retry:true" -y 
+    
 }
-
+function Install-Ubuntu{
+    choco install wsl-ubuntu-2004 -y
+}
 function Install-Media {
-    choco install spotify discord -y
+    choco install spotify -y 
+    choco install discord -y 
+    choco install obs-studio -y 
+    choco install obs-move-transition -y
 }
 
 function Install-GameServices {
-    choco install steam epicgameslauncher -y
+    choco install steam -y 
+    choco install steamcmd -y
+    choco install epicgameslauncher -y
 }
 
 Install-Tools
