267ee68f37ef762696123a7b4456816ac5178c36
Jørund Martinsen <joerundmartinsen@hotmail.com>
2021-11-20 16:02:48 +0100

    Config: Added packages to choco install

--- config/choco-installs.ps1
+++ config/choco-installs.ps1
@@ -1,7 +1,20 @@
 function Install-Tools {
-    choco install brave nordpass vscode -y
+    choco install brave nordpass nordvpn vscode microsoft-windows-terminal voicemeeter -y
+}
+
+function Install-WSL2 {
+    choco install wsl2 --params "/Version:2 Retry:true" -y 
 }
 
 function Install-Media {
-    choco install spotify
+    choco install spotify discord -y
 }
+
+function Install-GameServices {
+    choco install steam epicgameslauncher -y
+}
+
+Install-Tools
+Install-Media
+Install-GameServices
+Install-WSL2
